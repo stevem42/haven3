608897d787c777f1970ae56629ee11e2ab7d20bf
stevem42 <elevated42@yahoo.com.au>
2022-03-02 08:52:11 +1000

    added prisma - poss fast refresh issue

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,3 +1,11 @@
 export default function index() {
-  return <div className="text-green-600">index</div>;
+  return (
+    <div className="text-green-600 flex">
+      index
+      <h1 className="font-bold underline p-10">Some text</h1>
+      <h3 className="text-red-600 underline font-bold p-10 m-10">
+        Test with padding
+      </h3>
+    </div>
+  );
 }
