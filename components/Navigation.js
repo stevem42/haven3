3dfd36dea9b14f8734941db143f694f3d28bc728
stevem42 <elevated42@yahoo.com.au>
2022-03-03 08:07:57 +1000

    Added basic nav

---
+++ components/Navigation.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Navigation() {
+  return (
+    <header className="bg-purple-400 py-4 mb-2">
+      <nav className="flex justify-between mx-2 ">
+        <div>
+          <Link href="/">
+            <a>Recipe Haven</a>
+          </Link>
+        </div>
+        <div>
+          <ul className="flex list-[none] space-x-2">
+            <li>
+              <Link href="recipes">
+                <a>Recipes</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/login">
+                <a>Login</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/logout">
+                <a>Logout</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -2,10 +2,13 @@ export default function index() {
   return (
     <div className="text-green-600 flex">
       index
-      <h1 className="font-bold underline p-10">Some text</h1>
+      <h1 className="font-bold underline p-10 text-blue-600">Some text</h1>
       <h3 className="text-red-600 underline font-bold p-10 m-10">
         Test with padding
       </h3>
+      <h2 className="text-lg-3"> and more text</h2>
+      <h2>Another test title</h2>
+      <h2>Another test title</h2>
     </div>
   );
 }
